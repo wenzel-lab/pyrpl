0cfad999e61adff130997ee3f045932be59511a5
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2020-04-17 13:23:29 +0200

    changed to use 32 bit counter

--- pyrpl/fpga/rtl/red_pitaya_clock_helpers.v
+++ pyrpl/fpga/rtl/red_pitaya_clock_helpers.v
@@ -8,13 +8,13 @@ adapted from https://www.fpga4student.com/2017/08/verilog-code-for-clock-divider
 */
 
 module red_pitaya_clk_div #(
-    parameter DIV = 28'd100
+    parameter DIV = 32'd125000000
 )(
     input clk_i,
     output reg clk_o = 0
 );
 
-reg[27:0] counter = 28'd0;
+reg [32-1:0] counter = 0;
 // The frequency of the output clk_out
 //  = The frequency of the input clk_in divided by DIVISOR
 // For example: Fclk_in = 50Mhz, if you want to get 1Hz signal to blink LEDs
@@ -22,10 +22,10 @@ reg[27:0] counter = 28'd0;
 // Then the frequency of the output clk_out = 50Mhz/50.000.000 = 1Hz
 always @(posedge clk_i)
     begin
-        counter <= counter + 28'd1;
+        counter <= counter + 32'd1;
         if (counter >=  (DIV-1)) begin
             clk_o <= ~clk_o;
-            counter <= 28'd0;
+            counter <= 32'd0;
         end
     end
 endmodule
