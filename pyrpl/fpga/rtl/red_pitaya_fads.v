4a587d8d877b5506554989bc54f79df4a91904fc
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2020-05-23 13:42:42 +0200

    renamed thresholds, added droplet counter not really droplet counter but rather counts up every adc_clk_i when sort_trig == 1

--- pyrpl/fpga/rtl/red_pitaya_fads.v
+++ pyrpl/fpga/rtl/red_pitaya_fads.v
@@ -11,7 +11,8 @@ by an external high voltage amplifier to sort fluorescent droplets.
 
 module red_pitaya_fads #(
     parameter RSZ = 14, // RAM size: 2^RSZ,
-    parameter DWT = 14  // data width thresholds
+    parameter DWT = 14, // data width thresholds
+    parameter MEM = 32  // data width RAM
 //    parameter signed low_threshold  = 14'b00000000001111,
 //    parameter signed high_threshold = 14'b00000011111111
 )(
@@ -24,7 +25,7 @@ module red_pitaya_fads #(
     output reg              sort_trig       ,   // Sorting trigger
 
     // System bus
-    input      [ 32-1: 0] sys_addr      ,  // bus saddress
+    input      [ 32-1: 0] sys_addr      ,  // bus address
     input      [ 32-1: 0] sys_wdata     ,  // bus write data
     input      [  4-1: 0] sys_sel       ,  // bus write byte select
     input                 sys_wen       ,  // bus write enable
@@ -34,13 +35,20 @@ module red_pitaya_fads #(
     output reg            sys_ack          // bus acknowledge signal
 );
 
-reg [DWT -1:0]low_threshold;
+// Registers for thresholds
+reg [DWT -1:0]droplet_threshold;
+reg [DWT -1:0]sorting_threshold;
 reg [DWT -1:0]high_threshold;
+//reg [MEM -1:0]width_threshold;
+
+// Registers for droplet counters;
+reg [MEM -1:0]droplets = 32'd0;
 
 always @(posedge adc_clk_i) begin
-    if ((adc_a_i > low_threshold) && (adc_a_i < high_threshold))
+    if ((adc_a_i > sorting_threshold) && (adc_a_i < high_threshold))
     begin
         sort_trig <= 1;
+        droplets <= droplets + 32'd1;
     end else begin
         sort_trig <= 0;
     end
@@ -52,11 +60,13 @@ assign sys_en = sys_wen | sys_ren;
 
 always @(posedge adc_clk_i)
     if (adc_rstn_i == 1'b0) begin
-        low_threshold   <= 14'b00000000001111;
-        high_threshold  <= 14'b00000011111111;
+        sorting_threshold   <= 14'b00000000001111;
+        high_threshold      <= 14'b00000011111111;
+//        droplets            <= 32'd0;
     end else if (sys_wen) begin
-        if (sys_addr[19:0]==20'h00000)   low_threshold      <= sys_wdata[DWT-1:0];
-        if (sys_addr[19:0]==20'h00004)   high_threshold     <= sys_wdata[DWT-1:0];
+        if (sys_addr[19:0]==20'h00000)   sorting_threshold      <= sys_wdata[DWT-1:0];
+        if (sys_addr[19:0]==20'h00004)   high_threshold         <= sys_wdata[DWT-1:0];
+//        if (sys_addr[19:0]==20'h00008)   droplets               <= sys_wdata[MEM-1:0];
     end
 
 always @(posedge adc_clk_i)
@@ -66,9 +76,10 @@ always @(posedge adc_clk_i)
     end else begin
         sys_err <= 1'b0;
         casez (sys_addr[19:0])
-            20'h00000: begin sys_ack <= sys_en;  sys_rdata <= {{32- DWT{1'b0}}, low_threshold}  ; end
-            20'h00004: begin sys_ack <= sys_en;  sys_rdata <= {{32- DWT{1'b0}}, high_threshold} ; end
-            default:   begin sys_ack <= sys_en;  sys_rdata <= 32'h0                             ; end
+            20'h00000: begin sys_ack <= sys_en;  sys_rdata <= {{32- DWT{1'b0}}, sorting_threshold}  ; end
+            20'h00004: begin sys_ack <= sys_en;  sys_rdata <= {{32- DWT{1'b0}}, high_threshold}     ; end
+            20'h00008: begin sys_ack <= sys_en;  sys_rdata <= {{32- MEM{1'b0}}, droplets}           ; end
+            default:   begin sys_ack <= sys_en;  sys_rdata <= 32'h0                                 ; end
         endcase
     end
 
