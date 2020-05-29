372cca6e51c1e0a9f247a04a5d650d82529488c2
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2020-05-29 15:08:47 +0200

    set sort_enable to 1

--- pyrpl/fpga/rtl/red_pitaya_fads.v
+++ pyrpl/fpga/rtl/red_pitaya_fads.v
@@ -85,7 +85,7 @@ wire     high_width;
 
 // Maintenance
 reg droplet_acquisition_enable = 1'b1;
-reg sort_enable = 1'b0;
+reg sort_enable = 1'b1;
 reg [MEM -1:0] sort_counter = 32'd0;
 reg [MEM -1:0] sort_duration = 32'd125000;
 reg fads_reset = 1'b0;
